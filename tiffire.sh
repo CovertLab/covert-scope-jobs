9876a7083a49c51c9d556675f8ef7fcba6ab8080
Aditya Mukund <adityamukund@gmail.com>
2018-07-26 12:22:08 -0700

    added script to continuously run conversion process

---
+++ tiffire.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DELAY=28800 # wait 8 hours between runs
+
+while true
+do
+  START=$(date +%s)
+  bash robocop.sh
+  END=$(date +%s)
+  DELTA=$(($END-$START))
+  if [[ $DELTA -lt $DELAY ]]; then
+    SLEEP=$(($DELAY-$DELTA))
+  else
+    SLEEP=0
+  fi
+  sleep $SLEEP
+done
