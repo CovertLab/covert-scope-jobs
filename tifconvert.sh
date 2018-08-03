687e40c3901959bd51f9e5e7bc454c87b342e1f9
Aditya Mukund <adityamukund@gmail.com>
2018-08-03 15:30:41 -0700

    added metadata extraction, fixed flatfielding

--- tifconvert.sh
+++ tifconvert.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-cat robocopy.log | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | sed -e 's/^/\/mnt\/covert-lab\/instruments\/covert-lab-scope1/' > tifffiles.txt
 
 
-convert () {
+cat $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | sed -e 's/^/\/mnt\/covert-lab\/instruments\/covert-lab-scope1/' | sed -e 's/0%.*//' > tifffiles.txt
+
+
+fix () {
   local TIF=$1
   DIR="$(dirname "$TIF")"
-  FILE="$(basename "$TIF" .tif)"
-  PNG="${DIR}/$FILE.png"
+  FOL=${DIR:28}
+  FILE="$(basename "$TIF")"
+  PNG="${RESEARCHNAS}/instruments/${FOL}/${FILE}"
+  IN=${RESEARCHNAS}/instruments/${FOL}/${FILE}
+  OUT="${PNG::-4}.png"
 
-  python tiffix.py $TIF
-  convert $TIF $PNG
+  python tiffix.py $IN
+  convert $IN $OUT
 }
 
 while read TIF; do
-  convert $TIF & 
+  fix $TIF & 
 done < tifffiles.txt
--- tiffix.py
+++ tiffix.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python
 
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.ndimage import imread
 from os.path import join
-from glob import glob
+from glob import glob                                                                             
+import ast                                                                             
 import sys
 import os
 from os.path import join, dirname
 import tifffile as tiff
+from tifffile import TiffFile
+from urllib.request import urlretrieve
+import tempfile
+import shutil
 
 sys.path.append('/home/adi/covert/CellTK')
 sys.path.append('/home/adi/covert/CellTK/celltk')
 sys.path.append('/home/adi/covert/CellTK/celltk/utils')
-from preprocess_operation import shading_correction
+# from preprocess_operation import shading_correction
 
 from PIL import Image
 from PIL.TiffTags import TAGS
 import json
 
 
-refpath = 'http://archive.simtk.org/ktrprotocol/temp/ffref_{0}x{1}bin.npz'.format(magnification, binning)
-darkrefpath = 'http://archive.simtk.org/ktrprotocol/temp/ffdarkref_{0}x{1}bin.npz'.format(magnification, binning)
-ref, darkref = retrieve_ff_ref(refpath, darkrefpath)
+def retrieve_ff_ref(refpath, darkrefpath):
+    """
+    refpath: 'http://archive.simtk.org/ktrprotocol/temp/ffref_20x3bin.npz'
+    darkrefpath: 'http://archive.simtk.org/ktrprotocol/temp/ffdarkref_20x3bin.npz'
+    """
+    try:
+        temp_dir = tempfile.mkdtemp()
+        urlretrieve(refpath, join(temp_dir, 'ref.npz'))
+        ref = np.load(join(temp_dir, 'ref.npz'))
+        urlretrieve(darkrefpath, join(temp_dir, 'darkref.npz'))
+        darkref = np.load(join(temp_dir, 'darkref.npz'))
+    finally:
+        shutil.rmtree(temp_dir)  # delete directory
+    return ref, darkref
 
 def correct_shade(img, ref, darkref, ch):
     img = img.astype(np.float)
@@ -31,25 +46,39 @@ def correct_shade(img, ref, darkref, ch):
     return d1.mean() * d0/d1
 
 
+
 imgpath = sys.argv[1] # NOTE: assume full path e.g. /scratch/blah/blah/blah/image.tif
 
-with Image.open(imgpath) as img:
-    # metadata extraction
-    meta_dict = {TAGS[key] : img.tag[key] for key in img.tag.iterkeys()}
-    meta_dict['IMAGE_PATH'] = imgpath
-    meta_path = dirname(imgpath)+"/files_metadata.json"
-    with open(meta_path, "a") as j:
-        j.write(json.dumps(meta_dict))
 
-    #flatfielding
-    with open(join(dirname(imgpath), 'metadata.txt')) as mfile:
-        data = json.load(mfile)
-        channels = data['Summary']['ChNames']
-        for chnum, ch in enumerate(channels):
-            try:
-                img_sc = correct_shade(imread(path), ref, darkref, ch)
-            except:
-                img_sc = img
-    tiff.imsave(imgpath, img_sc.astype(np.float32))
+# metadata extraction
+with TiffFile(imgpath) as tif:
+    md = tif.imagej_metadata
+    info = ast.literal_eval(md['Info'])
+    try:
+        binning = int(info['Neo-Binning']['PropVal'][0])
+    except:
+        binning = 3
+    try:
+        magnification = int(info['TINosePiece-Label']['PropVal'][11:13])
+    except:
+        magnification = 20
 
+# flatfielding
+refpath = 'http://archive.simtk.org/ktrprotocol/temp/ffref_{0}x{1}bin.npz'.format(magnification, binning)
+darkrefpath = 'http://archive.simtk.org/ktrprotocol/temp/ffdarkref_{0}x{1}bin.npz'.format(magnification, binning)
+try:
+    ref, darkref = retrieve_ff_ref(refpath, darkrefpath)
+except:
+    refpath = 'http://archive.simtk.org/ktrprotocol/temp/ffref_{0}x{1}bin.npz'.format(20, 3)
+    darkrefpath = 'http://archive.simtk.org/ktrprotocol/temp/ffdarkref_{0}x{1}bin.npz'.format(20, 3)
+    ref, darkref = retrieve_ff_ref(refpath, darkrefpath)
 
+with open(join(dirname(imgpath), 'metadata.txt')) as mfile:
+    data = json.load(mfile)
+    channels = data['Summary']['ChNames']
+    for chnum, ch in enumerate(channels):
+        try:
+            img_sc = correct_shade(tiff.imread(imgpath), ref, darkref, ch)
+        except:
+            img+sc = tiff.imread(imgpath)
+    tiff.imsave(imgpath, np.array(img_sc, np.float32))
