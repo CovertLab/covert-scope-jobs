41740bb1445cb7473f276478a34f232555efa9da
kudo <kudo@stanford.edu>
2018-08-27 10:03:53 -0700

    channel retrieved from filters

--- tifconvert.sh
+++ tifconvert.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 RESEARCHNAS=~/covert-lab
+
+
+
 SCOPEDIR=$RESEARCHNAS/instruments/covert-lab-scope1
 ROBOCOPY=$SCOPEDIR/logs/robocopy.log
-# cat $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | sed -e 's/^/\/mnt\/covert-lab\/instruments\/covert-lab-scope1/' | sed -e 's/0%.*//' > tifffiles0.txt
 cat $ROBOCOPY | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | cut -f5 | cut -d" " -f1 | sed -e 's@[a-Z]*:@'"$SCOPEDIR"'@g' > tifffiles.txt
 mv $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy_old.log
 
@@ -16,7 +18,7 @@ fix () {
   # PNG="${RESEARCHNAS}/instruments/${FOL}/${FILE}"
   # IN=${RESEARCHNAS}/instruments/${FOL}/${FILE}
 
-  echo $IN
+  # echo $IN
   python tiffix.py $IN
   # OUT="${PNG::-4}.png"
   # convert $IN $OUT
