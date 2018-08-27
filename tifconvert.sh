0dbbc81e2e306f5d136997afce937053ffef778d
kudo <kudo@stanford.edu>
2018-08-26 18:41:28 -0700

    sed fixed

--- tifconvert.sh
+++ tifconvert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
-
-cat $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | sed -e 's/^/\/mnt\/covert-lab\/instruments\/covert-lab-scope1/' | sed -e 's/0%.*//' > tifffiles.txt
+RESEARCHNAS=~/covert-lab
+SCOPEDIR=$RESEARCHNAS/instruments/covert-lab-scope1
+ROBOCOPY=$SCOPEDIR/logs/robocopy.log
+# cat $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | sed -e 's/^/\/mnt\/covert-lab\/instruments\/covert-lab-scope1/' | sed -e 's/0%.*//' > tifffiles0.txt
+cat $ROBOCOPY | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | cut -f5 | cut -d" " -f1 | sed -e 's@[a-Z]*:@'"$SCOPEDIR"'@g' > tifffiles.txt
 mv $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy_old.log
 
 fix () {
