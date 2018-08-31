6d4aed7554a32dd872e9709a362f978ad0ccfdcc
braysia <kudo@stanford.edu>
2018-08-31 15:51:40 -0700

    use grep version

--- tifconvert.sh
+++ tifconvert.sh
@@ -5,11 +5,15 @@ SCOPEDIR=$RESEARCHNAS/instruments/covert-lab-scope1
 ROBOCOPY=$SCOPEDIR/logs/robocopy.log
 
 if [ ! -f tifffiles.txt ]; then
-  cat $ROBOCOPY | sed 's/\r//' | sed 's/.*m://' | sed -n '/tif/p' | sed 's/\\/\//g' | cut -f5 | cut -d" " -f1 | sed -e 's@[a-Z]*:@'"$SCOPEDIR"'@g' > tifffiles.txt
+  grep -I .tif $ROBOCOPY|grep -v ERROR | sed 's/\\/\//g'|cut -f5|sed 's/.tif.*/.tif/' | sed -e 's@[a-Z]*:@'"$SCOPEDIR"'@g' > tifffiles.txt
   sudo mv $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy.log $RESEARCHNAS/instruments/covert-lab-scope1/logs/robocopy_old.log
   
   sudo ~/.pyenv/shims/python tiffix.py tifffiles.txt 
 
   mv tifffiles.txt tifffiles_old.txt
 fi
+
+while read line; do
+rm $line
+done < corrupted.txt
 
